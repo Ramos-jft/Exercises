ac64a9093ce17e4b99387ddabbfe21d392866c8e
Lucas <sphlucasramos@gmail.com>
2025-08-13 14:20:13 -0300

    Add project files.

---
+++ Basic/Program.cs
@@ -0,0 +1,17 @@
+﻿using Basic;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exercicio = new Exercise01();
+
+            Console.Write("Digite seu nome: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(exercicio.Message(name));
+        }
+    }
+}
---
+++ BasicTests/Exercise01Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Basic;
+
+namespace BasicTests
+{
+    [TestClass]
+    public class Exercise01Tests
+    {
+        [TestMethod]
+        public void ShouldReturnHelloAndName()
+        {
+            // Arrange
+
+            var exercise = new Exercise01();
+
+            // Act
+
+            var result = exercise.Message("Lucas");
+
+            // Assert
+
+            Assert.AreEqual("Hello\nLucas", result);
+        }
+    }
+}
